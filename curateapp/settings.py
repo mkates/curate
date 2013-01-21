88a6c7535b77bca6c54be1e19a781e6726fb1365
Mitchell Kates <mkates@mit.edu>
2013-01-21 13:45:02 -0500

    fullcommit

--- curateapp/settings.py
+++ curateapp/settings.py
@@ -1,6 +1,6 @@
 # Django settings for curateapp project.
 import os
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -57,17 +57,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static'),
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/'),
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
 #ADMIN_MEDIA_PREFIX = os.path.join(PROJECT_ROOT,'website/admin')
-
+print os.path.join(PROJECT_ROOT,'static')
 # Additional locations of static files
 STATICFILES_DIRS = (
 	os.path.join(PROJECT_ROOT,'static'),
+	
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
