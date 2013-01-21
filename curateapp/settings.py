efa86eecc0ebfb37d25255379db41281ef2f6c33
Mitchell Kates <mkates@mit.edu>
2013-01-21 15:02:12 -0500

    wtf

--- curateapp/settings.py
+++ curateapp/settings.py
@@ -1,20 +1,21 @@
-# Django settings for curateapp project.
-import os
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-print PROJECT_ROOT
+# Django settings for coverage project.
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+print PROJECT_ROOT
 ADMINS = (
-     ('Mitchell Kates', 'mhkates@gmail.com'),
+    ('Mitchell Kates', 'mkates@mit.edu'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', 			 # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -58,14 +59,13 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static'),
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-#ADMIN_MEDIA_PREFIX = os.path.join(PROJECT_ROOT,'website/admin')
-print os.path.join(PROJECT_ROOT,'static')
+#ADMIN_MEDIA_PREFIX = os.path.join(PROJECT_ROOT,'coverage/admin')
 # Additional locations of static files
 STATICFILES_DIRS = (
 	os.path.join(PROJECT_ROOT,'static'),
@@ -83,7 +83,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '+k_68j#6%#qu&amp;o_in9*c64je)vik&amp;tb5fcw*nh(*y9*q)!ey3-'
+SECRET_KEY = '#@femhay!*u7es%hq1&amp;p5a(pghtbeiwo7gf&amp;zgkox$2v-zmlt3bh'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'curateapp.urls'
+ROOT_URLCONF = 'curateaoo.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'curateapp.wsgi.application'
@@ -121,8 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -155,6 +154,5 @@ LOGGING = {
         },
     }
 }
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
