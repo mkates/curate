6bfde9fb3fa9cf33b1e96e4bb21ab8cb0a447877
Mitchell Kates <mkates@mit.edu>
2013-01-21 22:07:04 -0500

    please work

--- curateapp/settings.py
+++ curateapp/settings.py
@@ -5,7 +5,6 @@ TEMPLATE_DEBUG = DEBUG
 
 import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-print PROJECT_ROOT
 ADMINS = (
     ('Mitchell Kates', 'mkates@mit.edu'),
 )
@@ -15,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '', 			 # Or path to database file if using sqlite3.
+        'NAME': '/users/mitchellkates/curate/curateapp.db', 			 # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -59,16 +58,17 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static'),
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-#ADMIN_MEDIA_PREFIX = os.path.join(PROJECT_ROOT,'coverage/admin')
+#ADMIN_MEDIA_PREFIX = os.path.join(PROJECT_ROOT,'curateapp/admin')
+
 # Additional locations of static files
 STATICFILES_DIRS = (
-	os.path.join(PROJECT_ROOT,'static'),
+	os.path.join(PROJECT_ROOT,'../website/static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -79,7 +79,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+  #  'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -99,7 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'curateapp.urls'
@@ -123,7 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'website'
 )
 
 # A sample logging configuration. The only tangible logging
--- curateapp/urls.py
+++ curateapp/urls.py
@@ -1,23 +1,23 @@
 from django.conf.urls import patterns, include, url
 from website import views
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('website.views',
     # Examples:
      url(r'^$','index'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 from django.conf import settings
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
-    )
+	urlpatterns += patterns('',
+		url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+	)
