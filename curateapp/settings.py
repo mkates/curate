affea906b69174f328172138c095278c2394bf34
Mitchell Kates <mkates@mit.edu>
2013-01-22 20:41:01 -0500

    production settings

--- curateapp/settings.py
+++ curateapp/settings.py
@@ -163,5 +163,5 @@ LOGGING = {
         },
     }
 }
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
